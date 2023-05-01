cf838ee132b6e1b6155c79f20bb03715db3fcdea
0csis <121513547+0csis@users.noreply.github.com>
2023-05-01 15:39:53 -0300

    add image to homepage

--- src/home.js
+++ src/home.js
@@ -1,14 +1,22 @@
 import './style.css';
+import Restaurant from './restaurant.jpg';
 
 const home = () => {
   const content = document.querySelector('#content');
 
   const homepage = document.createElement('div');
   homepage.classList.add('homepage');
+
   const header = document.createElement('header');
+  header.textContent = 'Nane International';
   homepage.appendChild(header);
 
-  header.textContent = 'Nane International';
+  const restaurantImage = document.createElement('div');
+  restaurantImage.classList.add('restaurantImage');
+  const myRestaurant = new Image();
+  myRestaurant.src = Restaurant;
+  restaurantImage.appendChild = myRestaurant;
+  homepage.appendChild(restaurantImage);
 
   content.appendChild(homepage);
 };
